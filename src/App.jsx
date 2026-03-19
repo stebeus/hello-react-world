7b2fed406ccd0ab029d45be3dd553b68b8e4c08e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-19 08:49:03 -0300

    feat: add Greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
 
+function Greeting() {
+  return <h1>This is a greeeeeeeeeeeeeeeeeeeeeeeeting</h1>;
+}
+
 function App() {
-  return <>HELLO WORLD</>;
+  return (
+    <>
+      HELLO WORLD
+      <Greeting></Greeting>
+    </>
+  );
 }
 
 export default App;
