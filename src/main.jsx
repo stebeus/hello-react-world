25c3fba5d2ea2bca14f2411db72c4277c8bd81d7
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-19 09:06:51 -0300

    feat: add ConvertedHtmlElement component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import Greeting from './components/Greeting.jsx';
 import { Egg } from './components/Food.jsx';
+import ConvertedHtmlElement from './components/ConvertedHtmlElement.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <App />
     <Greeting />
     <Egg />
+    <ConvertedHtmlElement />
   </StrictMode>,
 );
