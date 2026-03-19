df3d85d3f9311845596780b1cc3c1be4dd6713bd
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-19 09:00:36 -0300

    feat: add Food component

---
+++ src/components/Food.jsx
@@ -0,0 +1,10 @@
+function Egg() {
+  return (
+    <img
+      src='https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg'
+      alt='Three eggs with funny faces on a table'
+    />
+  );
+}
+
+export { Egg };
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Greeting from './components/Greeting.jsx';
+import { Egg } from './components/Food.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
+    <Egg />
   </StrictMode>,
 );
