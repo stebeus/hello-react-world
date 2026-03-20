3d500c736d39a35db177b869d3df79f7fdfb4723
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-20 09:26:32 -0300

    refactor!: move component calls to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Greeting from './components/Greeting.jsx';
+import { Egg } from './components/Food.jsx';
+import ConvertedHtmlElement from './components/ConvertedHtmlElement.jsx';
 
 function App() {
   return (
     <>
       HELLO WORLD
-      <Greeting></Greeting>
+      <Greeting />
+      <Egg />
+      <ConvertedHtmlElement />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Greeting from './components/Greeting.jsx';
-import { Egg } from './components/Food.jsx';
-import ConvertedHtmlElement from './components/ConvertedHtmlElement.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
-    <Egg />
-    <ConvertedHtmlElement />
+    <App />
   </StrictMode>,
 );
